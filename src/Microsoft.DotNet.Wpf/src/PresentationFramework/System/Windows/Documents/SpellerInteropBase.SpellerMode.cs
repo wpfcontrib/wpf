5d826473c0be306fcd8c300de58a8c145dc1c75c
Vatsan Madhavan <vatsan.madhavan@microsoft.com>
2020-01-03 14:37:46 -0800

    Refactor nested types in SpellerInteropBase into separate files.

---
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Documents/SpellerInteropBase.SpellerMode.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+
+namespace System.Windows.Documents
+{
+    internal abstract partial class SpellerInteropBase
+    {
+        [Flags]
+        internal enum SpellerMode
+        {
+            None                          = 0x0000,
+            WordBreaking                  = 0x0001, 
+            SpellingErrors                = 0x0002, 
+            Suggestions                   = 0x0004,
+            SpellingErrorsWithSuggestions = SpellingErrors | Suggestions, 
+            All                           = WordBreaking | SpellingErrorsWithSuggestions,
+        };
+    }
+}
